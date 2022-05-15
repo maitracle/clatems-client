501efe0193abe897081c3934f209fad3774ca7e2
maitracle <39800081+maitracle@users.noreply.github.com>
2022-05-15 14:02:01 +0900

    test socket

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,12 +3,26 @@ import PageWrapper from 'components/layouts/PageWrapper'
 import { fetchArtworkList } from 'store/Artwork/actions'
 import ArtworkList from 'modules/ArtworkList'
 import Gnb from 'components/layouts/Gnb'
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
 
 
 const Home = () => {
+  const asdf = () => {
+    console.log("clicked");
+
+    var socket = new SockJS('/ws');
+    let stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, () => {}, () => {});
+  }
   return (
     <PageWrapper>
       <Gnb isMainPage title={{ isLogo: true, text: '' }} />
+
+      <button onClick={asdf}>
+        click
+      </button>
       <ArtworkList fetchAction={fetchArtworkList} />
     </PageWrapper>
   )
